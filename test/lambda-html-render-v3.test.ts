d4a7bbd54dc3477ac6be62eb6f018478a9173712
Stephen Barr <stephen@vertexdev.com.au>
2025-03-13 17:43:12 -0700

    Initial commit

---
+++ test/lambda-html-render-v3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaHtmlRenderV3 from '../lib/lambda-html-render-v3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-html-render-v3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaHtmlRenderV3.LambdaHtmlRenderV3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
